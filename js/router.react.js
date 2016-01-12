a1f7549b7f866af410e19601294b65b6d04a3730
Andrew Odewahn <andrew@odewahn.com>
2016-01-12 15:36:02 -0500

    added babel transformations for es6

--- js/router.react.js
+++ js/router.react.js
@@ -14,16 +14,26 @@ var Route = Router.Route;
 var DefaultRoute = Router.DefaultRoute;
 var NotFoundRoute = Router.NotFoundRoute;
 
+let f = (x) => x*x
+
 // Declare routes
-var routes = (
-  <Route handler={App} path="/">
+let routes = <Route handler={App} path="/">
     <DefaultRoute name="app" handler={Hello} />
     <Route name="hello" handler={Hello} />
     <Route name="goodbye" handler={Goodbye} />
     <Route name="edit" path="/edit/:name" handler={Edit} />
     <NotFoundRoute handler={NotFound} />
   </Route>
-);
+
+/*
+const routes = (
+  <Route component={App}>
+    <Route path="/hello" handler={Hello} />
+    <Route path="/goodbye" component={Goodbye} />
+    <Route path="/" component={Hello} />
+  </Route>
+)
+*/
 
 Router.run(routes, function (Handler, state) {
   //var params = state.params;
